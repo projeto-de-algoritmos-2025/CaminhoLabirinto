81627305e923b06dc1771f5190ffa5e38bc6ad04
Arthur D'Assumpção Loureiro <dassumpcaoarthur@gmail.com>
2025-04-17 13:04:43 -0300

    adicionar shadcn buttons e lucide-react icons

--- src/components/maze.tsx
+++ src/components/maze.tsx
@@ -1,5 +1,7 @@
 "use client";
 import { useState, useEffect } from "react";
+import { Button } from "./ui/button";
+import { Flag, MapPin, RotateCcw, Trash2 } from "lucide-react";
 
 type CellType = "empty" | "wall" | "startPoint" | "endPoint";
 
@@ -26,47 +28,53 @@ export default function Maze() {
     }
     setGrid(newGrid);
   };
-  const startVisualization = () => {};
-  const resetVisualization = () => {};
+  const startVisualization = () => { };
+  const resetVisualization = () => { };
   return (
     <div className="flex flex-col items-center">
       <div className="flex flex-wrap gap-2 mb-4 justify-center">
-        <button
+        <Button
           onClick={() => setDrawMode("wall")}
+          variant={drawMode === "wall" ? "default" : "outline"}
           className="flex items-center gap-1 cursor-pointer"
-        >
+        > <Trash2 className="h-4 w-4" />
           Parede
-        </button>
-        <button
+        </Button>
+        <Button
           onClick={() => setDrawMode("startPoint")}
+          variant={drawMode === "startPoint" ? "default" : "outline"}
           className="flex items-center gap-1 cursor-pointer"
         >
-          Início
-        </button>
-        <button
+          <MapPin className="h-4 w-4" />Início
+        </Button>
+        <Button
           onClick={() => setDrawMode("endPoint")}
+          variant={drawMode === "endPoint" ? "default" : "outline"}
           className="flex items-center gap-1 cursor-pointer"
         >
-          Fim
-        </button>
-        <button
+          <Flag className="h-4 w-4" /> Fim
+        </Button>
+        <Button
           onClick={startVisualization}
           className="flex items-center gap-1 cursor-pointer"
         >
           Iniciar
-        </button>
-        <button
+        </Button>
+        <Button
           onClick={resetVisualization}
+          variant="outline"
           className="flex items-center gap-1 cursor-pointer"
         >
-          Reiniciar
-        </button>
-        <button
+          <RotateCcw className="h-4 w-4" />  Reiniciar
+        </Button>
+        <Button
           onClick={initializeGrid}
+          variant="destructive"
           className="flex items-center gap-1 cursor-pointer"
         >
-          Resetar
-        </button>
+          <Trash2 className="h-4 w-4" />
+          Apagar tudo
+        </Button>
       </div>
       <div className="border border-black bg-gray-100 p-1 rounded-xl shadow-md">
         <div
@@ -78,10 +86,10 @@ export default function Maze() {
         >
           {grid.map((row, rowIndex) =>
             row.map((cell, colIndex) => (
-              <button
+              <Button
                 key={`${rowIndex}-${colIndex}`}
                 className={`w-10 h-10 sm:w-10 sm:h-10 cursor-pointer bg-white`}
-                onClick={() => {}}
+                onClick={() => { }}
               />
             ))
           )}
